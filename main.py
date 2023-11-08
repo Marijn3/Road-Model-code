6ce248de0ec57db48ab7dc43957dd54e3f33f248
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-11-08 13:55:38 +0100

    Created dataframe for point data in extent

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ filepath = "data/Divergenties/divergenties.dbf"
 
 data = gpd.read_file(filepath)
 
+print(data['IBN'].unique())
 data.drop(columns=['FK_VELD4', 'IBN'], inplace=True)
 print(data.columns)
 print(data.head())
 
-data['geometry'] = data['geometry'].astype(str)
-data[['x', 'y']] = data['geometry'].str.extract(r'POINT \((\d+\.\d+) (\d+\.\d+)\)').astype(float)
+data['x'] = data['geometry'].apply(lambda point: point.x)
+data['y'] = data['geometry'].apply(lambda point: point.y)
 
 print(data.head())
 
@@ -21,13 +22,10 @@ south = 405851.2010
 west = 148277.3982
 extent = (west, south, east, north)
 
-# for i in data.index:
-#     ls = ExtractLineStringCoordinates(data_A2['geometry'][i])
-#     print(ls)
-#     data_A2.loc[i, 'inextent'] = CheckLineInExtent(ls, extent)
-#
-# print(data_A2.head())
-# print(data_A2[data_A2['inextent'] == True].head())
+xmin, ymin, xmax, ymax = extent
 
+data['inextent'] = (data['x'] >= xmin) & (data['x'] <= xmax) & (data['y'] >= ymin) & (data['y'] <= ymax)
 
+dataInExtent = data[data['inextent']]
 
+print(dataInExtent)
