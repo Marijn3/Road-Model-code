3bcbf4920819c3f3a8a817ed94826e53f6916d12
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-11-08 10:50:15 +0100

    Added extract coordinate function

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 
 import geopandas as gpd
+import shapely
 
 filepath = "data/Rijstroken/rijstroken.dbf"
 
-# Read file using gpd.read_file()
 data = gpd.read_file(filepath)
 
-print(data.head(2))
+
+def ExtractLineStringCoordinates(linestring: shapely.LineString) -> list[list[int, int]]:
+    return list(linestring.coords)
+
+
+# print(data.columns)
+data_A2 = data[data['WEGNUMMER'] == '002']
+# print(data_A2.head())
+
+ls = data_A2['geometry'][3]
+linegeometry = ExtractLineStringCoordinates(ls)
+print(linegeometry)
