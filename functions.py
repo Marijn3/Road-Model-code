0dfa2ef3a28ae40bd48a3594e5fff1feda4724d6
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-11-09 16:19:11 +0100

    Separation into load and selection function added

--- functions.py
+++ functions.py
@@ -3,10 +3,23 @@ from shapely import *
 import geopandas as gpd
 
 
-def FindDataInExtent(filepath: str, extent: shapely.box) -> gpd.GeoDataFrame:
+def LoadDataFrame(filepath: str) -> gpd.GeoDataFrame:
     data = gpd.read_file(filepath)
 
-    # Determine if data intersects extent
-    data['inextent'] = data['geometry'].apply(lambda geom: intersects(geom, extent))
+    # Remove unused columns
+    data.drop(columns=['FK_VELD4', 'IBN'], inplace=True)
 
+    # Determine the extent of the frame in RD-coordinates (example: Vught)
+    north = 409832.1696
+    east = 152987.0262
+    south = 405851.2010
+    west = 148277.3982
+    extent = box(xmin=west, ymin=south, xmax=east, ymax=north)
+
+    return SelectDataInExtent(data, extent)
+
+
+def SelectDataInExtent(data: gpd.GeoDataFrame, extent: shapely.box) -> gpd.GeoDataFrame:
+    # All data that intersects extent is considered 'in the extent'.
+    data['inextent'] = data['geometry'].apply(lambda geom: intersects(geom, extent))
     return data[data['inextent']]
--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 from functions import *
-from shapely import box
 
-fileConvergentie = "data/Convergenties/convergenties.dbf"
-fileDivergentie = "data/Divergenties/divergenties.dbf"
+fileConvergentie           = "data/Convergenties/convergenties.dbf"
+fileDivergentie            = "data/Divergenties/divergenties.dbf"
 fileRijstrooksignaleringen = "data/Rijstrooksignaleringen/strksignaleringn.dbf"
-fileMengstroken = "data/Mengstroken/mengstroken.dbf"
-fileRijstroken = "data/Rijstroken/rijstroken.dbf"
+fileMengstroken            = "data/Mengstroken/mengstroken.dbf"
+fileRijstroken             = "data/Rijstroken/rijstroken.dbf"
 
-# Determine the extent of the frame (Vught)
-north = 409832.1696
-east = 152987.0262
-south = 405851.2010
-west = 148277.3982
-extent = box(xmin=west, ymin=south, xmax=east, ymax =north)
+dfConvergentie           = LoadDataFrame(fileConvergentie)
+dfDivergentie            = LoadDataFrame(fileDivergentie)
+dfRijstrooksignaleringen = LoadDataFrame(fileRijstrooksignaleringen)
+dfMengstroken            = LoadDataFrame(fileMengstroken)
+dfRijstroken             = LoadDataFrame(fileRijstroken)
 
-dfConvergentie = FindDataInExtent(fileConvergentie, extent)
-dfDivergentie = FindDataInExtent(fileDivergentie, extent)
-dfRijstrooksignaleringen = FindDataInExtent(fileRijstrooksignaleringen, extent)
-dfMengstroken = FindDataInExtent(fileMengstroken, extent)
-dfRijstroken = FindDataInExtent(fileRijstroken, extent)
-
-# Dubbele entries rstrksgn verwijderen
+# Remove double entries
 dfRijstrooksignaleringen = dfRijstrooksignaleringen[dfRijstrooksignaleringen['CODE'] == 'KP']
-
-# Adapt columns
-# data.drop(columns=['FK_VELD4', 'IBN'], inplace=True)
 
 print(dfRijstroken[['geometry', 'inextent']].head())
