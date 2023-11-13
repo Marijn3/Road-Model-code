65d41a9994412f7b52dd6937d46499757d174768
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-11-13 14:52:23 +0100

    Privated most class functions.

--- functions.py
+++ functions.py
@@ -22,45 +22,45 @@ class DataFrameLoader:
         self.data = {}
         self.extent = None
 
-    # Use this function to load data for a specific location.
+    # Call this to load data for a specific location.
     def load_data_frames(self, location: str):
-        self.get_extent(location)
+        self.__get_extent(location)
         for file_path in self.file_paths:
-            df_layer_name = self.get_layer_name(file_path)
-            self.data[df_layer_name] = self.load_data_frame(file_path)
+            df_layer_name = self.__get_layer_name(file_path)
+            self.data[df_layer_name] = self.__load_data_frame(file_path)
 
-    def load_data_frame(self, file_path: str) -> gpd.GeoDataFrame:
+    def __load_data_frame(self, file_path: str) -> gpd.GeoDataFrame:
         data = gpd.read_file(file_path)
-        self.drop_columns(data)
-        return self.select_data_in_extent(data)
+        self.__drop_columns(data)
+        return self.__select_data_in_extent(data)
 
-    def get_extent(self, location: str) -> shapely.box:
-        coords = self.load_extent_from_csv(location)
+    def __get_extent(self, location: str) -> shapely.box:
+        coords = self.__load_extent_from_csv(location)
 
         if coords:
             self.extent = box(xmin=coords["west"], ymin=coords["south"], xmax=coords["east"], ymax=coords["north"])
         else:
             raise ValueError(f"Invalid place: {location}")
 
+    def __select_data_in_extent(self, data: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+        # All data that intersects extent is considered 'in the extent'.
+        data['inextent'] = data['geometry'].apply(lambda geom: geom.intersects(self.extent))
+        return data[data['inextent']]
+
     @staticmethod
-    def get_layer_name(file_path) -> str:
+    def __get_layer_name(file_path) -> str:
         # Folder name is extracted as the name.
         parts = file_path.split("/")
         folder_name = parts[-2]
         return folder_name
 
     @staticmethod
-    def drop_columns(data: gpd.GeoDataFrame):
+    def __drop_columns(data: gpd.GeoDataFrame):
         # These columns are unused
         data.drop(columns=['FK_VELD4', 'IBN'], inplace=True)
 
-    def select_data_in_extent(self, data: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
-        # All data that intersects extent is considered 'in the extent'.
-        data['inextent'] = data['geometry'].apply(lambda geom: geom.intersects(self.extent))
-        return data[data['inextent']]
-
     @staticmethod
-    def load_extent_from_csv(location: str) -> dict:
+    def __load_extent_from_csv(location: str) -> dict:
         with open('data/locations.csv', 'r') as file:
             csv_reader = csv.DictReader(file, delimiter=';')
             for row in csv_reader:
