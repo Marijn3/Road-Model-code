52f507af0ebe520bb434a47a1c569f16b5576c17
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-11-13 10:30:00 +0100

    Import extents from separate csv file.

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import shapely
 from shapely import *
 import geopandas as gpd
+import csv
 
 
 class DataFrameLoader:
@@ -16,8 +17,8 @@ class DataFrameLoader:
         self.extent = None
 
     # Use this function to load data for a specific location.
-    def load_data_frames(self, place: str):
-        self.get_extent(place)
+    def load_data_frames(self, location: str):
+        self.get_extent(location)
         for file_path in self.file_paths:
             df_layer_name = self.get_layer_name(file_path)
             self.data[df_layer_name] = self.load_data_frame(file_path)
@@ -27,18 +28,13 @@ class DataFrameLoader:
         self.drop_columns(data)
         return self.select_data_in_extent(data)
 
-    def get_extent(self, place: str) -> shapely.box:
-        definedplaces = {
-            "Vught": {"north": 409832, "east": 152987, "south": 405851, "west": 148277},
-            "A2VK": {"north": 348725, "east": 192000, "south": 331159, "west": 182000},
-            # TODO: Add more places and coordinates.
-        }
+    def get_extent(self, location: str) -> shapely.box:
+        coords = self.load_extent_from_csv(location)
 
-        if place in definedplaces:
-            coords = definedplaces[place]
+        if coords:
             self.extent = box(xmin=coords["west"], ymin=coords["south"], xmax=coords["east"], ymax=coords["north"])
         else:
-            raise ValueError(f"Invalid place: {place}")
+            raise ValueError(f"Invalid place: {location}")
 
     @staticmethod
     def get_layer_name(file_path) -> str:
@@ -56,3 +52,17 @@ class DataFrameLoader:
         # All data that intersects extent is considered 'in the extent'.
         data['inextent'] = data['geometry'].apply(lambda geom: geom.intersects(self.extent))
         return data[data['inextent']]
+
+    @staticmethod
+    def load_extent_from_csv(location: str) -> dict:
+        with open('data/locations.csv', 'r') as file:
+            csv_reader = csv.DictReader(file, delimiter=';')
+            for row in csv_reader:
+                if row['location'] == location:
+                    return {
+                        'north': float(row['north']),
+                        'east': float(row['east']),
+                        'south': float(row['south']),
+                        'west': float(row['west']),
+                    }
+        return {}  # Return empty dictionary if the location is not found
