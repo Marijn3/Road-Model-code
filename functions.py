a57d93b5026d6c0ad701c1ac4208b5e60e708ea9
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-11-13 08:50:18 +0100

    Added getExtent function with extent library

--- functions.py
+++ functions.py
@@ -9,14 +9,22 @@ def LoadDataFrame(filepath: str) -> gpd.GeoDataFrame:
     # Remove unused columns
     data.drop(columns=['FK_VELD4', 'IBN'], inplace=True)
 
-    # Determine the extent of the frame in RD-coordinates (example: Vught)
-    north = 409832.1696
-    east = 152987.0262
-    south = 405851.2010
-    west = 148277.3982
-    extent = box(xmin=west, ymin=south, xmax=east, ymax=north)
-
+    extent = getExtent("Vught")
     return SelectDataInExtent(data, extent)
+
+
+def getExtent(place: str) -> shapely.box:
+    definedplaces = {
+        "Vught": {"north": 409832, "east": 152987, "south": 405851, "west": 148277},
+        "A2VK": {"north": 348725, "east": 192000, "south": 331159, "west": 182000},
+        # TODO: Add more places and coordinates.
+    }
+
+    if place in definedplaces:
+        coords = definedplaces[place]
+        return box(xmin=coords["west"], ymin=coords["south"], xmax=coords["east"], ymax=coords["north"])
+    else:
+        raise ValueError(f"Invalid place: {place}")
 
 
 def SelectDataInExtent(data: gpd.GeoDataFrame, extent: shapely.box) -> gpd.GeoDataFrame:
