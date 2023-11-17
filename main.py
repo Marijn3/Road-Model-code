03eb72a47a0c64dc67d2e7ca373c9d3f951073d6
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-11-17 12:28:50 +0100

    Proof that AU and KP appear as pairs

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from functions import *
 # Load all files and store the GeoDataFrames in the class
 df = DataFrameLoader()
 df.load_data_frames("Vught")
-df.edit_data()
+# df.edit_data()
 
-# print(df.data['Convergenties'])
-# print(df.data['Rijstrooksignaleringen'])
-# print(df.data['Rijstroken'][['geometry', 'inextent']].head())
+# Temporary: Proof that AU and KP are always paired.
+dfAU = df.data['Rijstrooksignaleringen'][df.data['Rijstrooksignaleringen']['CODE'] == 'AU']
+dfKP = df.data['Rijstrooksignaleringen'][df.data['Rijstrooksignaleringen']['CODE'] == 'KP']
+
+dfAU_lim = dfAU.sort_values(by=['geometry']).reset_index().drop(columns=['index', 'CODE', 'OMSCHR'])
+dfKP_lim = dfKP.sort_values(by=['geometry']).reset_index().drop(columns=['index', 'CODE', 'OMSCHR'])
+
+print(dfAU_lim)
+print(dfKP_lim)
+
+print(all(dfAU_lim == dfKP_lim))
