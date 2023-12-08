704b44ca5c7c860708615bb4c5a8b9dc52ab7a09
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-12-08 09:25:28 +0100

    Added simple test file

---
+++ test.py
@@ -0,0 +1,38 @@
+import unittest
+import pandas as pd
+from functions import *
+
+
+class TestRoadModel(unittest.TestCase):
+
+    def test_import_dataframes(self):
+        road_model = RoadModel()
+        dfl = DataFrameLoader()
+
+        # Add test data
+        rijstroken_data = pd.DataFrame({'IZI_SIDE': ['L', 'L'],
+                                        'BEGINKM': [0, 1],
+                                        'EINDKM': [1, 2],
+                                        'nLanes': [2, 2],
+                                        'geometry': [shapely.LineString([[0, 0], [1, 0], [1, 1]])
+                                                     ]})
+
+        kantstroken_data = pd.DataFrame({'IZI_SIDE': ['L', 'L'],
+                                         'BEGINKM': [0, 1],
+                                         'EINDKM': [1, 2],
+                                         'Vluchtstrook': [True, False],
+                                         'Spitsstrook': [False, False],
+                                         'Puntstuk': [False, True],
+                                         'geometry': [shapely.LineString([[0, 0], [1, 0], [1, 1]])
+                                                      ]})
+
+        dfl.data = {'Rijstroken': rijstroken_data, 'Kantstroken': kantstroken_data}
+
+        road_model.import_dataframes(dfl)
+
+        self.assertEqual(len(road_model.sections), 1)
+        # TODO: Add more assertions...
+
+
+if __name__ == '__main__':
+    unittest.main()
