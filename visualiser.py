5dd57c6680e1b4231b7430dc78d4457ed0321bbe
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2023-12-19 16:29:03 +0100

    Used viewbox instead of manually relocating all coordinates

--- visualiser.py
+++ visualiser.py
@@ -2,28 +2,16 @@ from functions import *
 import svgwrite
 
 LANE_WIDTH = 5
-offset_x = 148000
-offset_y = 407000
-scale_factor = 0.2
-
-
-def move_coord(c: tuple) -> tuple:
-    return (c[0] - offset_x) * scale_factor, (c[1] - offset_y) * scale_factor
 
 
 def shapely_to_svg(geom, prop, svg_elem):
     if isinstance(prop['nLanes'], int):
         road_width = LANE_WIDTH*prop['nLanes']
-        # print([type(coord) for coord in geom.coords])
-        line_coordinates = [move_coord(coordinate) for coordinate in geom.coords]
-        roadline = svgwrite.shapes.Polyline(points=line_coordinates, stroke='grey', fill="none", stroke_width=road_width)
-        svg_elem.add(roadline)
+        roadline = svgwrite.shapes.Polyline(points=geom.coords, stroke='grey', fill="none", stroke_width=road_width)
     else:
         road_width = LANE_WIDTH * 2
-        line_coordinates = [move_coord(coordinate) for coordinate in geom.coords]
-        roadline = svgwrite.shapes.Polyline(points=line_coordinates, stroke='red', fill="none",
-                                            stroke_width=road_width)
-        svg_elem.add(roadline)
+        roadline = svgwrite.shapes.Polyline(points=geom.coords, stroke='red', fill="none", stroke_width=road_width)
+    svg_elem.add(roadline)
 
 
 # Load all files and store the GeoDataFrames in the class
@@ -40,7 +28,15 @@ sections = road.get_sections()
 for section in sections:
     shapely_to_svg(section['geometry'], section['properties'], svg_drawing)
 
-#print(dfl.extent)
+corner1_x, corner1_y = get_coordinates(dfl.extent)[0]
+corner2_x, corner2_y = get_coordinates(dfl.extent)[2]
+
+# Adjust the viewBox to relocate the visible portion of the SVG
+viewbox_x = (corner1_x + corner2_x)/2
+viewbox_y = (corner1_y + corner2_y)/2
+viewbox_width = abs(corner2_x - corner1_x)
+viewbox_height = abs(corner2_y - corner1_y)
+svg_drawing.viewbox(minx=viewbox_x, miny=viewbox_y, width=viewbox_width, height=viewbox_height)
 
 # Save SVG file
 svg_drawing.save()
