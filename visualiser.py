2c1e619bd1221ee010a0bb8dfe5b3695e8a000ea
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-01-18 14:33:07 +0100

    Introduced different line type visualisation

--- visualiser.py
+++ visualiser.py
@@ -17,7 +17,7 @@ RATIO = VIEWBOX_HEIGHT / VIEWBOX_WIDTH
 
 
 def get_road_color(prop: dict) -> str:
-    if 'Vluchtstrook' in prop.values():
+    if 'Puntstuk' in prop.values():
         return 'dimgrey'
 
     # if not isinstance(prop['nRijstroken'], int):
@@ -28,6 +28,7 @@ def get_road_color(prop: dict) -> str:
 
 def get_n_lanes(prop: dict) -> int:
     n_lanes = max([lane_number for (lane_number, lane_type) in prop.items() if (isinstance(lane_number, int) and lane_type not in ['Puntstuk'])])
+    print(n_lanes)
     return n_lanes
 
 
@@ -41,10 +42,10 @@ def get_transformed_coords(geom: LineString | Point) -> list[tuple]:
 
 def get_offset_coords(geom: LineString, offset: float) -> list[tuple]:
     if offset == 0:
-        return [(point[0], TOP_LEFT_Y - (point[1] - TOP_LEFT_Y)) for point in geom.coords]
+        return [(coord[0], TOP_LEFT_Y - (coord[1] - TOP_LEFT_Y)) for coord in geom.coords]
     else:
-        geom2 = offset_curve(geom, offset, join_style="mitre", mitre_limit=5)
-        return [(point[0], TOP_LEFT_Y - (point[1] - TOP_LEFT_Y)) for point in geom2.coords]
+        offset_geom = offset_curve(geom, offset, join_style="mitre", mitre_limit=5)
+        return [(coord[0], TOP_LEFT_Y - (coord[1] - TOP_LEFT_Y)) for coord in offset_geom.coords]
 
 
 def svg_add_section(geom: LineString, prop: dict, svg_dwg: svgwrite.Drawing):
@@ -60,32 +61,61 @@ def svg_add_section(geom: LineString, prop: dict, svg_dwg: svgwrite.Drawing):
 
 def add_separator_lines(geom: LineString, prop: dict, svg_dwg: svgwrite.Drawing):
     n_lanes = get_n_lanes(prop)
+    print(prop)
 
     # Offset centered around 0
     offsets = [LANE_WIDTH*i - (LANE_WIDTH*n_lanes)/2 for i in range(0, n_lanes+1)]
 
-    # Add 'left' solid line
-    line_coords = get_offset_coords(geom, offsets.pop(0))
-    add_markerline(line_coords, svg_dwg)
-
-    # Add 'right' solid line
-    line_coords = get_offset_coords(geom, offsets.pop(-1))
-    add_markerline(line_coords, svg_dwg)
-
-    # Add remaining dashed lines
-    for offset in offsets:
+    for index, offset in enumerate(offsets):
+        road_nr = index + 1
         line_coords = get_offset_coords(geom, offset)
-        add_markerline(line_coords, svg_dwg, "dashed")
+
+        # Add first line (left).
+        if road_nr == 1:
+            print('first lane: solid')
+            add_markerline(line_coords, svg_dwg)
+
+        # Stop when this is the final roadline (right).
+        if road_nr == n_lanes:
+            print('final lane: solid. stop.')
+            add_markerline(line_coords, svg_dwg)
+            break
+
+        if road_nr in prop:
+            this_type = prop[road_nr]
+        else:
+            print('skip2')
+            continue
+        if road_nr + 1 in prop:
+            next_type = prop[road_nr + 1]
+        else:
+            print('skip1')
+            continue
+
+        # An emergency lane is always the final, rightmost lane.
+        if next_type == 'Vluchtstrook':
+            print('vluchtstrook lane: solid. stop.')
+            add_markerline(line_coords, svg_dwg)
+            break
+
+        # All other lanes are separated by dashed lines.
+        if this_type == next_type:
+            print('same types: dashed')
+            add_markerline(line_coords, svg_dwg, "dashed")
+        # If the lane types are not the same, block markings are used.
+        else:
+            print('different types: block')
+            add_markerline(line_coords, svg_dwg, "block")
 
 
 def add_markerline(coords: list[tuple], svg_dwg: svgwrite.Drawing, linetype: str = "full"):
-    stroke = 0.3
+    stroke = 0.4
     if linetype == "dashed":
         line = svgwrite.shapes.Polyline(points=coords, stroke="white", fill="none", stroke_width=stroke,
-                                        stroke_dasharray="6")
+                                        stroke_dasharray="2.5")
     elif linetype == "block":
         line = svgwrite.shapes.Polyline(points=coords, stroke="white", fill="none", stroke_width=stroke,
-                                        stroke_dasharray="0.5 2")
+                                        stroke_dasharray="0.4 1.5")
     else:
         line = svgwrite.shapes.Polyline(points=coords, stroke="white", fill="none", stroke_width=stroke)
     svg_dwg.add(line)
