3bbf06304b062313b36da55fca7bbe478a9522a5
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-01-22 13:46:45 +0100

    Minor changes to main for demo

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from functions import *
 dfl = DataFrameLoader()
 dfl.load_dataframes("Vught")
 
+# Construct a road model using the GeoDataFrames
 road = RoadModel()
 road.import_dataframes(dfl)
 
 # Inspect...
-road.get_properties_at(121.6, 'L')
-road.get_properties_at(121.8, 'L')
-road.get_properties_at(110.9, 'R')
-road.get_properties_at(121.6, 'R')
+road.get_properties_at(121.6, 'L')  # Two sections
+road.get_properties_at(121.8, 'L')  # One section
+road.get_properties_at(110.9, 'L')  # No sections
+road.get_properties_at(121.6, 'R')  # Other side of road
