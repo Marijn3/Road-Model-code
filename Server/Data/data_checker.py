1e5cf3e0231f1d21001734085cb42a7f2720b9b2
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-02-24 12:36:48 +0100

    Fixed the data_checker file by taking out (old) 'lanes' variable.

--- Server/Data/data_checker.py
+++ Server/Data/data_checker.py
@@ -1,7 +1,8 @@
 import json
 
-data_file = "Data/a16_a58/data_a16_a58.json"
-log_file = "Data/log.txt"
+data_file = "../../output_data.json"
+data_file = "a16_a58/data_a16_a58.json"
+log_file = "log.txt"
 only_composite_relations = True
 
 if not only_composite_relations:
@@ -29,24 +30,27 @@ if only_composite_relations:
         "Upstream_Narrowing"
     ]
 
-def getRelations(road_layout,RSU,MSI,relation_type):
-    [type_1,type_2] = relation_type.split("_")
 
-    temp = road_layout[RSU]["Lanes"][MSI][type_1][type_2]
+def getRelations(road_layout, RSU, MSI, relation_type):
+    [type_1, type_2] = relation_type.split("_")
+
+    temp = road_layout[RSU]["MSI"][MSI][type_1][type_2]
     if temp:
         [RSU_2,MSI_2] = temp.split(",")
         return f"({RSU}_{MSI}, {RSU_2[1:]}_{MSI_2[:-1]})   {type_1} {type_2}"
-    else: return None
+    else:
+        return None
+
 
 with open(data_file) as json_file:
     road_layout = json.load(json_file)
 
-source_file = open(log_file,'w')
+source_file = open(log_file, 'w')
 for i in relation_type:
     for RSU in road_layout:
-            for MSI in road_layout[RSU]["Lanes"]:
-                relations = getRelations(road_layout,RSU,MSI,i)
-                if relations:
-                    print(relations, file = source_file)
+        for MSI in road_layout[RSU]["MSI"]:
+            relations = getRelations(road_layout, RSU, MSI, i)
+            if relations:
+                print(relations, file=source_file)
 source_file.close
 
