8b6014ff79d89029535d13a043bf47b7faf79c7c
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-02-27 17:25:52 +0100

    Added name conversion functions for MTM -> JvM

--- ilp_input_creator.py
+++ ilp_input_creator.py
@@ -41,30 +41,62 @@ msi_row_dict = {
 }
 
 
+def transform_name(name: str) -> list | str | None:
+    """
+    Transform an MTM name to a name used as input for the ILP problem.
+    Also takes lists of names and calls itself.
+    Args:
+        name (str): MTM form of name.
+    Returns:
+        Name in the form used by JvM.
+    """
+    if name is None:
+        return None
+
+    if isinstance(name, list):
+        return [transform_name(n) for n in name]
+
+    road, km, msi_nr = name.split(":")
+    return f"[ROW_{road[:-1]}_{road[-1]}_{km},{msi_nr}]"
+
+
+def transform_row_name(name: str) -> list | str | None:
+    """
+    Transform an MTM row name to a row name used as input for the ILP problem.
+    Args:
+        name (str): MTM form of name.
+    Returns:
+        Name in the form used by JvM.
+    """
+    road, km = name.split(":")
+    return f"ROW_{road[:-1]}_{road[-1]}_{km}"
+
+
 def make_ILP_input(network: MSINetwerk) -> dict:
     road_dict = {}
     for i_row, row in enumerate(network.MSIrows):
-        road_dict[row.name] = deepcopy(msi_row_dict)
+        row_name = transform_row_name(row.name)
+        road_dict[row_name] = deepcopy(msi_row_dict)
         for i_msi, msi in row.MSIs.items():
-            road_dict[row.name]["MSI"][msi.lane_number] = deepcopy(msi_dict)
-            road_dict[row.name]["MSI"][msi.lane_number]["Downstream"]["Primary"] = msi.properties["d"]
-            road_dict[row.name]["MSI"][msi.lane_number]["Downstream"]["Secondary"] = msi.properties["ds"]
+            road_dict[row_name]["MSI"][msi.lane_number] = deepcopy(msi_dict)
+            road_dict[row_name]["MSI"][msi.lane_number]["Downstream"]["Primary"] = transform_name(msi.properties["d"])
+            road_dict[row_name]["MSI"][msi.lane_number]["Downstream"]["Secondary"] = transform_name(msi.properties["ds"])
 
-            road_dict[row.name]["MSI"][msi.lane_number]["Upstream"]["Primary"] = msi.properties["u"]
-            road_dict[row.name]["MSI"][msi.lane_number]["Upstream"]["Secondary"] = msi.properties["us"]
+            road_dict[row_name]["MSI"][msi.lane_number]["Upstream"]["Primary"] = transform_name(msi.properties["u"])
+            road_dict[row_name]["MSI"][msi.lane_number]["Upstream"]["Secondary"] = transform_name(msi.properties["us"])
             # More to be added as MSI data expands.
             
-            road_dict[row.name]["MSI"][msi.lane_number]["Rush_hour_lane"] = msi.properties["RHL"]
-            road_dict[row.name]["MSI"][msi.lane_number]["Exit-Entry"] = msi.properties["Exit_Entry"]
+            road_dict[row_name]["MSI"][msi.lane_number]["Rush_hour_lane"] = msi.properties["RHL"]
+            road_dict[row_name]["MSI"][msi.lane_number]["Exit-Entry"] = msi.properties["Exit_Entry"]
 
-            road_dict[row.name]["MSI"][msi.lane_number]["TrafficStream"] = str(msi.properties["TS_num"])
-            road_dict[row.name]["MSI"][msi.lane_number]["TrafficStream_Influence"]["Left"] = msi.properties["DIF_V_left"]
-            road_dict[row.name]["MSI"][msi.lane_number]["TrafficStream_Influence"]["Right"] = msi.properties["DIF_V_right"]
-            road_dict[row.name]["MSI"][msi.lane_number]["Carriageway"] = str(msi.properties["CW_num"])
+            road_dict[row_name]["MSI"][msi.lane_number]["TrafficStream"] = str(msi.properties["TS_num"])
+            road_dict[row_name]["MSI"][msi.lane_number]["TrafficStream_Influence"]["Left"] = msi.properties["DIF_V_left"]
+            road_dict[row_name]["MSI"][msi.lane_number]["TrafficStream_Influence"]["Right"] = msi.properties["DIF_V_right"]
+            road_dict[row_name]["MSI"][msi.lane_number]["Carriageway"] = str(msi.properties["CW_num"])
 
-        road_dict[row.name]["Continue-V"] = msi.properties["C_V"]
-        road_dict[row.name]["Continue-X"] = msi.properties["C_X"]
-        road_dict[row.name]["Stat-V"] = msi.properties["STAT_V"]
+        road_dict[row_name]["Continue-V"] = msi.properties["C_V"]
+        road_dict[row_name]["Continue-X"] = msi.properties["C_X"]
+        road_dict[row_name]["Stat-V"] = msi.properties["STAT_V"]
 
     return road_dict
 
