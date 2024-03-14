077ceaebebb4adc69e2db64ee3a08073f2e4a4d2
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-03-14 09:53:32 +0100

    Added get_lanes_werkvak as separate function

--- safety.py
+++ safety.py
@@ -2,7 +2,7 @@ from functions import *
 
 
 class Oppervlak:
-    def __init__(self, km_start: float, km_end: float, width: list, roadside: str) -> None:
+    def __init__(self, roadside: str, km_start: float, km_end: float, width: list) -> None:
         self._km_start = km_start
         self._km_end = km_end
         self._width = width
@@ -11,23 +11,24 @@ class Oppervlak:
 
 
 class Werkvak(Oppervlak):
-    def __init__(self, km_start: float, km_end: float, lanes: list, roadside: str) -> None:
+    def __init__(self, roadside: str, km_start: float, km_end: float, lanes: list) -> None:
         width_1 = [(lane-1)*3.5 for lane in lanes]
         width_2 = [lane*3.5 for lane in lanes]
         widths = sorted(set(width_1 + width_2))
         width = [widths[0], widths[-1]]
-        super().__init__(km_start, km_end, width, roadside)
+        super().__init__(roadside, km_start, km_end, width)
         self._color = "cyan"
 
 
 class Aanvraag(Oppervlak):
-    def __init__(self, wegmodel: WegModel, km_start: float, km_end: float, wegkant: str, hectoletter: str = "",
+    def __init__(self, wegmodel: WegModel, wegkant: str, km_start: float, km_end: float, hectoletter: str = "",
                  ruimte_links: float = None, ruimte_midden: list = None, ruimte_rechts: float = None,
                  max_v: int = 70, maatregel_korter_24h: bool = True, afzetting: str = "Bakens") -> None:
         assert sum(1 for v in [ruimte_links, ruimte_midden, ruimte_rechts] if v is not None) == 1, "Specificeer één eis."
 
         self.wegmodel = wegmodel
-        self.km_range = [km_start, km_end]
+        self.km_start = km_start
+        self.km_end = km_end
         self.wegkant = wegkant
         self.hectoletter = hectoletter
         self.ruimte_links = ruimte_links
@@ -39,7 +40,7 @@ class Aanvraag(Oppervlak):
         self.color = "green"
         self.request_width = self.__determine_request_width()
 
-        super().__init__(km_start, km_end, self.request_width, wegkant)
+        super().__init__(self.wegkant, self.km_start, self.km_end, self.request_width)
 
         self.__make_werkvak()
 
@@ -55,7 +56,6 @@ class Aanvraag(Oppervlak):
             return [self.ruimte_rechts, self.ruimte_rechts + 3.5]
 
     def __make_werkvak(self):
-
         # Obtain surrounding geometry and road properties
         # Temporary assumption: only one section below request, section identified by km_start
         road_info = self.wegmodel.get_section_info_by_bps(km=self.km_start,
@@ -69,71 +69,66 @@ class Aanvraag(Oppervlak):
                       and lane_type not in ['Puntstuk']]
         n_lanes = len(road_lanes)
 
+        sphere_of_influence = 8.00  # Alpha: invloedssfeer van de weg TODO: Make dependent on road.
+
         # Obtain minimal number of lanes for werkvak according to request
-        lanes_werkvak = []
-        ALPHA = 8.00  # Alpha: invloedssfeer van de weg
-
-        if self.measure_shorter_than_day:
-            if self.ruimte_links:
-                if self.ruimte_links >= 3.50:
-                    print("Geen afzetting nodig.")
-                else:
-                    # Case TL1
-                    lanes_werkvak = [1]
-
-            if ruimte_midden:
-                a = min(ruimte_midden) - 1
-                b = n_lanes - max(ruimte_midden)
-                if a < b:
-                    # Case TL1
-                    lanes_werkvak = range(1, max(ruimte_midden) + 1)
-                else:
-                    # Case TR3
-                    lanes_werkvak = range(min(ruimte_midden), n_lanes + 1)
-
-            if ruimte_rechts:
-                if ruimte_rechts >= ALPHA:
-                    print("Geen afzettingen nodig.")
-                elif ruimte_rechts >= 1.10:
-                    # Case TR1
-                    lanes_werkvak = [n_lanes]
-                else:
-                    # Case TR2
-                    lanes_werkvak = [n_lanes - 1, n_lanes]
-        else:  # Measure longer than day
-            if ruimte_links:
-                if ruimte_links >= 0.25:
-                    # LL1
-                    lanes_werkvak = [1]
-                else:
-                    # Case LL2
-                    lanes_werkvak = [1, 2]
-
-            if ruimte_midden:
-                a = min(ruimte_midden) - 1
-                b = n_lanes - max(ruimte_midden)
-                if a < b:
-                    # Case LL3
-                    lanes_werkvak = range(1, max(ruimte_midden) + 1)
-                else:
-                    # Case SR4
-                    lanes_werkvak = range(min(ruimte_midden), n_lanes + 1)
-
-            if ruimte_rechts:
-                if ruimte_rechts >= 2.50:  # Alpha: invloedssfeer van de weg
-                    # Case SR1
-                    print("Geen afzettingen nodig.")
-                elif ruimte_rechts >= 1.00:
-                    # Case SR2
-                    lanes_werkvak = [n_lanes]
-                else:
-                    # Case SR3
-                    lanes_werkvak = [n_lanes - 1, n_lanes]
-
-        if not lanes_werkvak:
-            return
+        lanes_werkvak = self.__get_lanes_werkvak(n_lanes, sphere_of_influence)
 
         # Initialise werkvak
-        Werkvak(km_start, km_end, lanes_werkvak, wegkant)
+        Werkvak(self.wegkant, self.km_start, self.km_end, lanes_werkvak)
+
+    def __get_lanes_werkvak(self, n_lanes: int, sphere_of_influence: float) -> list:
+        if self.maatregel_korter_24h:
+            if self.ruimte_links:
+                if self.ruimte_links >= 3.50:
+                    print("Geen afzettingen nodig.")
+                    return []
+                else:
+                    return [1]  # Case TL1
+
+            if self.ruimte_midden:
+                a = min(self.ruimte_midden) - 1
+                b = n_lanes - max(self.ruimte_midden)
+                if a < b:
+                    return list(range(1, max(self.ruimte_midden) + 1))  # Case TL2
+                else:
+                    return list(range(min(self.ruimte_midden), n_lanes + 1))  # Case TR3
+
+            if self.ruimte_rechts:
+                if self.ruimte_rechts >= sphere_of_influence:
+                    print("Geen afzettingen nodig.")
+                    return []
+                elif self.ruimte_rechts >= 1.10:
+
+                    return [n_lanes]  # Case TR1
+                else:
+                    return [n_lanes - 1, n_lanes]  # Case TR2
+        else:  # Measure longer than day
+            if self.ruimte_links:
+                if self.ruimte_links >= 0.25:
+                    return [1]  # LL1
+                else:
+                    return [1, 2]  # Case LL2
+
+            if self.ruimte_midden:
+                a = min(self.ruimte_midden) - 1
+                b = n_lanes - max(self.ruimte_midden)
+                if a < b:
+                    return list(range(1, max(self.ruimte_midden) + 1))  # Case LL3
+                else:
+                    return list(range(min(self.ruimte_midden), n_lanes + 1))  # Case LR4
+
+            if self.ruimte_rechts:
+                if self.ruimte_rechts >= 2.50:  # Alpha: invloedssfeer van de weg
+                    print("Geen afzettingen nodig.")
+                    return []  # Case LR1
+                elif self.ruimte_rechts >= 1.00:
+                    return [n_lanes]  # Case LR2
+                else:
+                    return [n_lanes - 1, n_lanes]  # Case LR3
+
+        print("Deze code zou nooit moeten worden bereikt.")
+        return []
+
 
 
