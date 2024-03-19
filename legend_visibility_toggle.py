4cd159b8643cee5279a110c3f876b3ec07fccab9
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-03-19 11:35:49 +0100

    Tested changing svg from outside - not working yet because of xml tags

--- legend_visibility_toggle.py
+++ legend_visibility_toggle.py
@@ -16,6 +16,6 @@ def toggle_visibility(svg_file, group_id, visible):
 
 # Example usage
 svg_file = 'Server/Data/WEGGEG/road_visualization.svg'
-group_id = 'group1'
+group_id = 'r[A58L:38.839:2]'
 visible = False  # Set visibility to False to hide the group
 toggle_visibility(svg_file, group_id, visible)
