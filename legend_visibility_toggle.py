d7312aac43dc0b3aa1d81d627aa6e1e0463a791c
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-03-19 12:17:14 +0100

    Enabled multiple legends to be set at once

--- legend_visibility_toggle.py
+++ legend_visibility_toggle.py
@@ -1,21 +1,25 @@
 import xml.etree.ElementTree as ET
 
 
-def toggle_visibility(svg_file, group_id, visible):
+def toggle_visibility(svg_file, group_visibility):
     tree = ET.parse(svg_file)
     root = tree.getroot()
 
-    # Find the group element with the specified ID
-    group = root.find(".//*[@id='{}']".format(group_id))
+    for group_id, visible in group_visibility.items():
+        # Find the group element with the specified ID
+        group = root.find(".//*[@id='{}']".format(group_id))
 
-    if group is not None:
-        group.attrib["visibility"] = "visible" if visible else "hidden"
-        # Specify that namespaces (ns0) should not be written out
-        ET.register_namespace('', 'http://www.w3.org/2000/svg')
-        tree.write(svg_file, xml_declaration=True, method='xml', encoding='utf-8')
+        if group is not None:
+            group.attrib["visibility"] = "visible" if visible else "hidden"
+
+    # Specify that namespaces (ns0) should not be written out
+    ET.register_namespace('', 'http://www.w3.org/2000/svg')
+    tree.write(svg_file, xml_declaration=True, method='xml', encoding='utf-8')
 
 
 svg_file = 'Server/Data/WEGGEG/road_visualization.svg'
-group_id = 'z[A58L:38.839:2]'
-visible = True
-toggle_visibility(svg_file, group_id, visible)
+group_visibility = {
+    'x[A58R:38.839:2]': True,
+    'x[A58R:38.839:3]': True,
+}
+toggle_visibility(svg_file, group_visibility)
