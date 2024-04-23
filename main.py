f23d4888fff5404ea51ceb9721954bb802be02eb
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-04-23 13:20:12 +0200

    Re-introduced safety request in main file

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from road_model import DataFrameLader, WegModel
 from msi_relations import MSINetwerk
 from visualiser import SvgMaker
 from ilp_input_creator import make_ILP_input, generate_file
-# from safety import Aanvraag
+from safety import Aanvraag
 
 ILP_ROADMODEL_FOLDER = "Server/Data/RoadModel"
-MSI_RELATIONS_OUTPUT = "msi_relations.txt"
+MSI_RELATIONS_FILE = "msi_relations.txt"
 
 # ========= Gedefinieerde locaties =========
 # Volledig correcte import : Vught, Oosterhout, Goirle, Vinkeveen, A27
@@ -16,7 +16,7 @@ MSI_RELATIONS_OUTPUT = "msi_relations.txt"
 # * = Na het oplossen van registratiefouten
 
 # Laad WEGGEG-bestanden in voor een gedefinieerd gebied, of voer coordinaten in.
-dfl = DataFrameLader("Vught", "data/locaties.csv")
+dfl = DataFrameLader("Oosterhout", "data/locaties.csv")
 # dfl = DataFrameLader({"noord": 433158.9132, "oost": 100468.8980, "zuid": 430753.1611, "west": 96885.3299},
 #                      "data/locaties.csv")
 
@@ -25,22 +25,22 @@ wegmodel = WegModel(dfl)
 
 # Bepaal MSI relaties en eigenschappen gebaseerd op het wegmodel.
 MSIs = MSINetwerk(wegmodel, maximale_zoekafstand=2000, alle_secundaire_relaties=True)
-MSIs.make_print(MSI_RELATIONS_OUTPUT)
+MSIs.make_print(MSI_RELATIONS_FILE)
 
 # Maak een visualisatie van het wegmodel en de afgeleide MSI-relaties.
-SvgMaker(wegmodel, MSI_RELATIONS_OUTPUT, f"{ILP_ROADMODEL_FOLDER}/RoadModelVisualisation.svg", 1000, False)
+SvgMaker(wegmodel, MSI_RELATIONS_FILE, f"{ILP_ROADMODEL_FOLDER}/RoadModelVisualisation.svg", 1000, False)
 
 # Exporteer de MSI-eigenschappen naar een bestand.
-ilp_input = make_ILP_input(MSIs, MSI_RELATIONS_OUTPUT)
+ilp_input = make_ILP_input(MSIs, MSI_RELATIONS_FILE)
 generate_file(ilp_input, f"{ILP_ROADMODEL_FOLDER}/LSC.json")
 
 # # Instantieer een aanvraag (A27 Oosterhout)
-# aanvraag = Aanvraag(wegmodel,
-#                     km_start=13.95,
-#                     km_end=13.98,
-#                     wegkant="R",
-#                     korter_dan_24h=True,
-#                     # ruimte_links=1,
-#                     ruimte_midden=[1],
-#                     # ruimte_rechts=1.5,
-#                     )
+aanvraag = Aanvraag(wegmodel,
+                    km_start=13.95,
+                    km_end=13.98,
+                    wegkant="R",
+                    korter_dan_24h=True,
+                    # ruimte_links=1,
+                    ruimte_midden=[1],
+                    # ruimte_rechts=1.5,
+                    )
