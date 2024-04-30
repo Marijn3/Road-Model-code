f068cf2bded55fd2597bdcbfb2b559a3b2b5cf79
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-04-30 08:34:46 +0200

    First version of CGGTOP-reading file

---
+++ msi_relation_destillation.py
@@ -0,0 +1,23 @@
+CGGTOP_FOLDER = "data/ZUIDNEDERLAND.00295"
+MSI_RELATIONS_OUTPUT = "msi_relations_mtm.txt"
+
+with open(CGGTOP_FOLDER, "r") as cggtop_file:
+    for line in cggtop_file.readlines():
+        if line.rstrip() == "!SECTION DESCRIPTION CLOSED":
+            print("End this section")
+        elif line[0:20] == "!SECTION DESCRIPTION":
+            roadnumber = line.strip("!SECTION DESCRIPTION ").rstrip()
+            print(f"Start this section: {roadnumber}")
+
+        if line[0:2] == " !":
+            # New onderstation
+            print(line.strip())
+            onderstation_data = line.strip().split(sep=" ")
+            km_text = onderstation_data[0][1:]
+            km = float(km_text.replace(",", "."))
+            roadnumber = onderstation_data[3]
+            print(roadnumber, ":", km)
+
+
+
+
