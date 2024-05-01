9a8ccd34c11d8c01c6694f867da67a847cf74246
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-05-01 16:26:33 +0200

    Added barebones for all steps from report into the new file.

--- Server/safety_areas.py
+++ Server/safety_areas.py
@@ -44,38 +44,89 @@ class Aanvraag:
         self.werkvak = Werkvak(self)
 
         self.step_1_determine_minimal_werkruimte()
-
         self.step_2_determine_area_sizes()
+        self.step_3_determine_legend_request()
+        self.step_4_solve_legend_request()
+        self.step_5_adjust_area_sizes()
 
     def step_1_determine_minimal_werkruimte(self):
         self.werkruimte.determine_minimal_werkruimte_size()
 
     def step_2_determine_area_sizes(self):
-        return
+        # Determine minimal width from the original area
+        self.veiligheidsruimte.adjust_edges_to_werkruimte()
+        self.werkvak.adjust_edges_to_veiligheidsruimte()
+
+        # Determine convenient width in regard to the road
+        self.werkvak.adjust_edges_to_road()
+        self.veiligheidsruimte.adjust_edges_to_werkvak()
+        self.werkruimte.adjust_edges_to_veiligheidsruimte()
+
+        # Determine minimal length in regard to the MSIs
+        self.werkvak.adjust_length_to_msis()
+        self.veiligheidsruimte.adjust_length_to_werkvak()
+        self.werkruimte.adjust_length_to_veiligheidsruimte()
+
+    def step_3_determine_legend_request(self):
+        return  # TODO
+
+    def step_4_solve_legend_request(self):
+        return  # TODO
+    
+    def step_5_adjust_area_sizes(self):
+        return  # TODO
 
 
 class Werkruimte:
     def __init__(self, request: Aanvraag) -> None:
-        self.request = request
         self.surface_type = WERKRUIMTE
-        self.edge_left = self.request.edge_left
-        self.edge_right = self.request.edge_right
+        self.request = request
+        self.edge_left = request.edge_left
+        self.edge_right = request.edge_right
+        self.km = request.km
 
     def determine_minimal_werkruimte_size(self):
-        return
+        return  # TODO
+
+    def adjust_edges_to_veiligheidsruimte(self):
+        return  # TODO
+
+    def adjust_length_to_veiligheidsruimte(self):
+        return  # TODO
 
 
 class Veiligheidsruimte:
     def __init__(self, request: Aanvraag) -> None:
-        self.request = request
         self.surface_type = VEILIGHEIDSRUIMTE
-        self.edge_left = self.request.edge_left
-        self.edge_right = self.request.edge_right
+        self.request = request
+        self.edge_left = request.edge_left
+        self.edge_right = request.edge_right
+        self.km = request.km
+
+    def adjust_edges_to_werkruimte(self):
+        return  # TODO
+
+    def adjust_edges_to_werkvak(self):
+        return  # TODO
+
+    def adjust_length_to_werkvak(self):
+        return  # TODO
 
 
 class Werkvak:
     def __init__(self, request: Aanvraag) -> None:
-        self.request = request
         self.surface_type = WERKVAK
-        self.edge_left = self.request.edge_left
-        self.edge_right = self.request.edge_right
+        self.request = request
+        self.edge_left = request.edge_left
+        self.edge_right = request.edge_right
+        self.km = request.km
+
+    def adjust_edges_to_veiligheidsruimte(self):
+        return  # TODO
+
+    def adjust_edges_to_road(self):
+        return  # TODO
+
+    def adjust_length_to_msis(self):
+        return  # TODO
+
