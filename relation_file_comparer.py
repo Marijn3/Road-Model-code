669b129cc9fcdbb18cedd6551bfa0fda46efe736
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-05-16 09:07:17 +0200

    Added script to generate structured logfile

--- relation_file_comparer.py
+++ relation_file_comparer.py
@@ -1,73 +1,101 @@
 msi_rel_file = "msi_relations.txt"
 mtm_rel_file = "msi_relations_mtm.txt"
 
-roadnrs = set()
-msi_rels = dict()
-found_log = list()
-not_found_log = list()
-lines_found = list()
+roadnumbers_in_roadmodel_dataset = set()
+roadmodel_msi_relations = dict()
+found_relations_log = list()
+not_found_relations_log = list()
+line_numbers_found = list()
+
+# --------------------------------------------
+# Load road model relations
+# --------------------------------------------
 
 with open(msi_rel_file, "r") as file:
-    orig_lines = file.readlines()
-    for index, line in enumerate(orig_lines):
-        msi_1, relation, msi_2 = line.strip().split(" ")
-        roadnr1, km1, lanenr1 = msi_1.split(":")
-        roadnr2, km2, lanenr2 = msi_2.split(":")
-        roadnrs.add(roadnr1)
-        roadnrs.add(roadnr2)
-        msi_rels[index] = {"roadnr1": roadnr1, "km1": km1, "lanenr1": lanenr1,
-                           "rel": relation,
-                           "roadnr2": roadnr2, "km2": km2, "lanenr2": lanenr2}
+    original_roadmodel_lines = file.readlines()
+    for index, line in enumerate(original_roadmodel_lines):
+        msi1, relation, msi2 = line.strip().split(" ")
+        roadnumber1, km1, lanenumber1 = msi1.split(":")
+        roadnumber2, km2, lanenumber2 = msi2.split(":")
+        roadnumbers_in_roadmodel_dataset.add(roadnumber1)
+        roadnumbers_in_roadmodel_dataset.add(roadnumber2)
+        roadmodel_msi_relations[index] = {"roadnr1": roadnumber1, "km1": km1, "lanenr1": lanenumber1,
+                                          "rel": relation,
+                                          "roadnr2": roadnumber2, "km2": km2, "lanenr2": lanenumber2}
 
-n_msi_rels = len(orig_lines)
+# --------------------------------------------
+# Load MTM-based MSI relations
+# --------------------------------------------
 
 with open(mtm_rel_file, "r") as file:
     orig_mtm_lines = file.readlines()
     for line in orig_mtm_lines:
-        if line.startswith(tuple(roadnrs)):
-            found_linking_msi = False
-            msi_1, relation, msi_2 = line.strip().split(" ")
-            roadnr1, km1, lanenr1 = msi_1.split(":")
-            roadnr2, km2, lanenr2 = msi_2.split(":")
-            for index, msi_rel in msi_rels.items():
-                eq1 = roadnr1 == msi_rel["roadnr1"]
-                eq2 = roadnr2 == msi_rel["roadnr2"]
-                eq3 = lanenr2 == msi_rel["lanenr1"]
-                eq4 = lanenr2 == msi_rel["lanenr2"]
-                eq5 = abs(float(km1) - float(msi_rel["km1"])) <= 0.02
-                eq6 = abs(float(km2) - float(msi_rel["km2"])) <= 0.02
+        found_linking_msi = False
+        # if line.startswith(tuple(roadnumbers_in_roadmodel_dataset)):
+        if any(roadnumber in line for roadnumber in tuple(roadnumbers_in_roadmodel_dataset)):
+            msi1, relation, msi2 = line.strip().split(" ")
+            roadnumber1, km1, lanenumber1 = msi1.split(":")
+            roadnumber2, km2, lanenumber2 = msi2.split(":")
+            for index, msi_rel in roadmodel_msi_relations.items():
+                km1_difference = round(abs(float(km1) - float(msi_rel["km1"])), 3)
+                km2_difference = round(abs(float(km2) - float(msi_rel["km2"])), 3)
+
+                eq1 = roadnumber1 == msi_rel["roadnr1"]
+                eq2 = roadnumber2 == msi_rel["roadnr2"]
+                eq3 = lanenumber2 == msi_rel["lanenr1"]
+                eq4 = lanenumber2 == msi_rel["lanenr2"]
+                eq5 = km1_difference <= 0.035
+                eq6 = km2_difference <= 0.035
 
                 if all([eq1, eq2, eq3, eq4, eq5, eq6]):
-                    found_log.append(f"{msi_1} {relation} {msi_2} links with {orig_lines[index].strip()}")
-                    lines_found.append(index)
+                    found_relations_log.append(
+                        f"{msi1} {relation} {msi2}  <->  {original_roadmodel_lines[index].strip()}  "
+                        f"(difference: {int(km1_difference * 1000)}m and {int(km2_difference * 1000)}m)"
+                    )
+                    line_numbers_found.append(index)
                     found_linking_msi = True
-            if not found_linking_msi:
-                not_found_log.append(f"{line.strip()} does not link with road model relations")
+                    break
+        if not found_linking_msi:
+            not_found_relations_log.append(line)
 
-lines_found.sort(reverse=True)
-print(lines_found)
+# --------------------------------------------
+# Process data
+# --------------------------------------------
 
-for i in lines_found:
-    orig_lines.pop(i)
+line_numbers_found.sort(reverse=True)
 
-if len(orig_lines) == 0:
-    print("yay!")
+roadmodel_lines = original_roadmodel_lines.copy()
 
-print([f"{line.strip()} does not link with any MTM relations" for line in orig_lines])
-print(found_log)
-print(not_found_log)
+for i in line_numbers_found:
+    roadmodel_lines.pop(i)
+
+if len(roadmodel_lines) == 0:
+    print("All relations matched. See log file for more details.")
+else:
+    print("Not all relations matched. See log file for more details.")
+
+# --------------------------------------------
+# Write comparison output log
+# --------------------------------------------
 
 with open("relation_comparison_log.txt", "w") as outfile:
-    outfile.write(f"Relation comparison log between {msi_rel_file} and {mtm_rel_file}.\n")
-    outfile.write(f"Found matches: {len(found_log)}\n")
-    outfile.write(f"Relations from road model without match: {len(orig_lines)}/{n_msi_rels}\n")
-    outfile.write(f"Relations from CGGTOP without match: {len(not_found_log)}/{len(orig_mtm_lines)}\n")
-    outfile.write(f"\nROAD MODEL:\n")
-    for line in orig_lines:
-        outfile.write(f"{line.strip()} does not link with any MTM relations\n")
+    outfile.write(f"Relation comparison log between {msi_rel_file} and {mtm_rel_file}.\n\n")
+    outfile.write(f"Found matches: {len(found_relations_log)}\n")
+    outfile.write(f"Relations from road model without match: {len(roadmodel_lines)}/{len(original_roadmodel_lines)}\n")
+    outfile.write(f"Relations from CGGTOP without match: {len(not_found_relations_log)}/{len(orig_mtm_lines)}\n")
+    outfile.write(f"\nFOUND MATCHES:\n")
+    for line in found_relations_log:
+        outfile.write(f"{line}\n")
 
+    outfile.write(f"\nROAD MODEL UNMATCHED LINES:\n")
+    if not roadmodel_lines:
+        outfile.write(f"-\n")
+    for line in roadmodel_lines:
+        outfile.write(f"{line.strip()}\n")
 
-
-
-
-
+    outfile.write(f"\nMTM UNMATCHED LINES (FILTERED BY ROAD NUMBER):\n")
+    if not not_found_relations_log:
+        outfile.write(f"-\n")
+    for line in not_found_relations_log:
+        if any(roadnr in line for roadnr in tuple(roadnumbers_in_roadmodel_dataset)):
+            outfile.write(f"{line.strip()}\n")
