ec34380e9e89043e5e53fa88dd189ccbed31379a
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-07-04 08:37:08 +0200

    Added initial code for step 5

--- safety_areas.py
+++ safety_areas.py
@@ -171,11 +171,15 @@ class Aanvraag:
         self.empty_space = EmptySpace(self)
         self.closed_space = ClosedSpace(self)
 
+        self.legend_pattern = []
+
+        self.final_workspaces = []
+        self.final_empty_spaces = []
+        self.final_closed_spaces = []
+
         self.step_1_determine_initial_workspace()
         self.step_2_determine_area_sizes()
         self.step_3_generate_measure_request()
-
-        self.report_request()
 
         self.step_4_solve_measure_request()
         # self.step_5_adjust_area_sizes()
@@ -212,6 +216,7 @@ class Aanvraag:
     def step_3_generate_measure_request(self) -> None:
         self.closed_space.obtain_msis_inside()
         self.closed_space.determine_measure_request()
+        self.report_request()
 
     def step_4_solve_measure_request(self) -> None:
         if not self.measure_request["legend_requests"]:
@@ -229,17 +234,83 @@ class Aanvraag:
 
         # Add code to communicate with ILP
         ilp = ILPSender()
-        response = ilp.send_request(scenario, {"SafetyAreaBasedRequest": self.measure_request})
-        logger.info(response)
+        self.legend_pattern = ilp.send_request(scenario, {"SafetyAreaBasedRequest": self.measure_request})
+        logger.info(f"Antwoord van ILP: {self.legend_pattern}")
 
         # Visualise response in svg
         svg_file = "Server/Data/RoadModel/RoadModelVisualisation.svg"
-        toggle_visibility(svg_file, response)
+        toggle_visibility(svg_file, self.legend_pattern)
         logger.info("Signaalgeverbeelden zijn toegevoegd aan de visualisatie.")
 
-
     def step_5_adjust_area_sizes(self) -> None:
-        return  # TODO
+        self.final_closed_spaces.append(self.closed_space)
+
+        cross_locations = {}
+
+        for legend in self.legend_pattern:
+            if legend[0] == "x":  # Filter for cross legends
+                lane_number = int(legend[-2])
+                km = float(legend[-9:-3])
+                if km in cross_locations.keys():
+                    cross_locations[km].append(lane_number)
+                else:
+                    cross_locations[km] = [lane_number]
+
+        # Post-processing on lane numbers
+        for km, lane_numbers in cross_locations.items():
+            adjusted_lanes = sorted(lane_numbers)
+            for index, lane in enumerate(adjusted_lanes):
+                if lane == 1:
+                    adjusted_lanes.insert(0, None)
+                if lane == self.last_main_lane_nr:
+                    adjusted_lanes.append(None)
+            cross_locations[km] = [adjusted_lanes[0], adjusted_lanes[-1]]
+
+        print(cross_locations)
+
+        km_registrations = sorted(cross_locations.keys(), reverse=self.roadside=="L")
+
+        change_analysis = []
+        km_string = []
+        for km in km_registrations:
+            lane_numbers = cross_locations[km]
+
+            if not km_string:
+                km_string.append(km)
+                current_lanes = lane_numbers
+                continue
+
+            if lane_numbers == current_lanes:
+                km_string.append(km)
+            else:
+                # Close previous round
+                km_range = [km_string[0], km_string[1]]
+                change_analysis.append((km_range, current_lanes))
+
+                # New round
+                km_string = [km]
+                current_lanes = lane_numbers
+
+        # Add final round
+        km_range = [km_string[0], km_string[1]]
+        change_analysis.append((km_range, current_lanes))
+
+        print(change_analysis)
+
+        for change_data in change_analysis:
+            km_range, lanes = change_data
+            print(km_range)
+            print(lanes)
+            if self.open_side == "L":
+                edges = {"L": Rand(rijstrook=lanes[0], afstand=+0.0),
+                         "R": Rand(rijstrook=lanes[1], afstand=+2.0)}
+            else:  # self.open_side == "R":
+                edges = {"L": Rand(rijstrook=lanes[0], afstand=-2.0),
+                         "R": Rand(rijstrook=lanes[1], afstand=-0.0)}
+            partial_closed_space = ClosedSpace(self, edges, km_range)
+            self.final_closed_spaces.append(partial_closed_space)
+
+        print(self.final_closed_spaces)
 
     def plot_areas(self) -> None:
         fig = plt.figure()
@@ -450,11 +521,11 @@ class EmptySpace:
 
 
 class ClosedSpace:
-    def __init__(self, request: Aanvraag) -> None:
+    def __init__(self, request: Aanvraag, edges: dict[str: Rand] = None, km: list[float] = None) -> None:
         self.surface_type = CLOSED_SPACE
         self.request = request
-        self.edges = deepcopy(request.edges)
-        self.km = request.km
+        self.edges = deepcopy(request.edges) if edges is None else edges
+        self.km = request.km if km is None else km
 
     def adjust_edges_to_empty_space(self) -> None:
         adjust_edges_to(self.request.empty_space, self, self.surface_type, away_from=True)
@@ -526,9 +597,9 @@ class ClosedSpace:
         self.request.msis_red_cross = msi_rows_inside
 
     def determine_measure_request(self) -> None:
-        # See report JvM page 71 for an explanation of this request structure
+        # See report J. van Meurs page 71 for an explanation of this request structure
         request_options = {
-            "name": "SafetyAreaBasedRequest",  # f"Request_{datetime.now().strftime('%H:%M:%S')}.{datetime.now().strftime('%f')[:3]}",
+            "name": "SafetyAreaBasedRequest",
             "type": "add",
             "legend_requests": [],
             "options": {}
