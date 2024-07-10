b92ffac28857f7246b02c0b483164ccbe42d75a6
Marijn Minkenberg <m.minkenberg@student.tue.nl>
2024-07-10 10:21:28 +0200

    Updated code that runs all tests

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,23 +1,26 @@
 from road_model import DataFrameLader, WegModel
 from msi_network import MSINetwerk
+from run_profiles import Run
 from utils import *
 import traceback
 logger = logging.getLogger(__name__)
 
-locaties = ["Vught", "Oosterhout", "Goirle", "Vinkeveen", "A27",
-            "Zonzeel", "Bavel", "Grijsoord", "Zuidasdok", "Everdingen", "A2VK", "Lankhorst", "Amstel"]
+run = Run(msi_relaties_overschrijven=True)
+
+profiles = [run.vught, run.oosterhout, run.goirle, run.vinkeveen, run.a27, run.zonzeel, run.bavel,
+            run.grijsoord, run.zuidasdok, run.everdingen, run.a2vk, run.lankhorst, run.amstel]
 
 n_gelukt = 0
 
-for locatie in locaties:
+for profile in profiles:
     try:
-        logger.info(f"{locatie} verwerken...")
-        dfl = DataFrameLader(locatie, "locaties.csv", "WEGGEG")
+        logger.info(f"{profile.name} verwerken...")
+        dfl = DataFrameLader(profile)
         wegmodel = WegModel(dfl)
-        MSIs = MSINetwerk(wegmodel, maximale_zoekafstand=2000, kruisrelaties=True)
+        MSIs = MSINetwerk(wegmodel)
     except Exception:
-        logger.error(f"Error bij het verwerken van {locatie}: {traceback.format_exc()}")
+        logger.error(f"Error bij het verwerken van {profile.name}: {traceback.format_exc()}")
     else:
         n_gelukt += 1
-        logger.info(f"{locatie} verwerkt.")
-logger.info(f"In totaal {n_gelukt} van de {len(locaties)} locaties verwerkt.")
+        logger.info(f"{profile.name} verwerkt.")
+logger.info(f"In totaal {n_gelukt} van de {len(profiles)} locaties verwerkt.")
